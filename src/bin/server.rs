312f6949fda536226ca0b12a0e2d082049a2aa0a
SwiftyOS <craigswift13@gmail.com>
2024-06-15 23:31:19 +0200

    Making a little progress

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,27 +1,7 @@
-use std::collections::HashMap;
-use std::sync::Arc;
-
-use russh::server::Server as _;
-use terminal_shop::ssh::Server;
-use tokio::sync::Mutex;
+use terminal_shop::ssh::AppServer;
 
 #[tokio::main]
 async fn main() {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Debug)
-        .init();
-
-    let config = russh::server::Config {
-        inactivity_timeout: Some(std::time::Duration::from_secs(3600)),
-        auth_rejection_time: std::time::Duration::from_secs(3),
-        auth_rejection_time_initial: Some(std::time::Duration::from_secs(0)),
-        keys: vec![russh_keys::key::KeyPair::generate_ed25519().unwrap()],
-        ..Default::default()
-    };
-    let config = Arc::new(config);
-    let mut sh = Server {
-        clients: Arc::new(Mutex::new(HashMap::new())),
-        id: 0,
-    };
-    sh.run_on_address(config, ("0.0.0.0", 2222)).await.unwrap();
+    let mut server = AppServer::new();
+    server.run().await.expect("Failed running server");
 }
--- src/ssh.rs
+++ src/ssh.rs
@@ -2,50 +2,132 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use russh::server::{Msg, Server as _, Session};
-use russh::*;
-use russh_keys::*;
+use ratatui::backend::CrosstermBackend;
+use ratatui::layout::Rect;
+use ratatui::style::{Color, Style};
+use ratatui::Terminal;
+use ratatui::{prelude::*, widgets::*};
+use russh::server::*;
+use russh::{Channel, ChannelId};
+use russh_keys::key::PublicKey;
 use tokio::sync::Mutex;
 
-#[tokio::main]
-async fn main() {
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Debug)
-        .init();
+type SshTerminal = Terminal<CrosstermBackend<TerminalHandle>>;
 
-    let config = russh::server::Config {
-        inactivity_timeout: Some(std::time::Duration::from_secs(3600)),
-        auth_rejection_time: std::time::Duration::from_secs(3),
-        auth_rejection_time_initial: Some(std::time::Duration::from_secs(0)),
-        keys: vec![russh_keys::key::KeyPair::generate_ed25519().unwrap()],
-        ..Default::default()
-    };
-    let config = Arc::new(config);
-    let mut sh = Server {
-        clients: Arc::new(Mutex::new(HashMap::new())),
-        id: 0,
-    };
-    sh.run_on_address(config, ("0.0.0.0", 2222)).await.unwrap();
+struct App {
+    pub counter: usize,
 }
 
-#[derive(Clone)]
-pub struct Server {
-    pub clients: Arc<Mutex<HashMap<(usize, ChannelId), russh::server::Handle>>>,
-    pub id: usize,
-}
-
-impl Server {
-    pub async fn post(&mut self, data: CryptoVec) {
-        let mut clients = self.clients.lock().await;
-        for ((id, channel), ref mut s) in clients.iter_mut() {
-            if *id != self.id {
-                let _ = s.data(*channel, data.clone()).await;
-            }
-        }
+impl App {
+    pub fn new() -> App {
+        Self { counter: 0 }
     }
 }
 
-impl server::Server for Server {
+fn centered_rect(r: Rect, percent_x: u16, percent_y: u16) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - percent_y) / 2),
+            Constraint::Percentage(percent_y),
+            Constraint::Percentage((100 - percent_y) / 2),
+        ])
+        .split(r);
+
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - percent_x) / 2),
+            Constraint::Percentage(percent_x),
+            Constraint::Percentage((100 - percent_x) / 2),
+        ])
+        .split(popup_layout[1])[1]
+}
+
+#[derive(Clone)]
+struct TerminalHandle {
+    handle: Handle,
+    // The sink collects the data which is finally flushed to the handle.
+    sink: Vec<u8>,
+    channel_id: ChannelId,
+}
+
+// The crossterm backend writes to the terminal handle.
+impl std::io::Write for TerminalHandle {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.sink.extend_from_slice(buf);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        let handle = self.handle.clone();
+        let channel_id = self.channel_id;
+        let data = self.sink.clone().into();
+        futures::executor::block_on(async move {
+            let result = handle.data(channel_id, data).await;
+            if result.is_err() {
+                eprintln!("Failed to send data: {:?}", result);
+            }
+        });
+
+        self.sink.clear();
+        Ok(())
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct AppServer {
+    clients: Arc<Mutex<HashMap<usize, (SshTerminal, App)>>>,
+    id: usize,
+}
+
+impl AppServer {
+    pub fn new() -> Self {
+        Self {
+            clients: Arc::new(Mutex::new(HashMap::new())),
+            id: 0,
+        }
+    }
+
+    pub async fn run(&mut self) -> Result<(), anyhow::Error> {
+        let clients = self.clients.clone();
+        tokio::spawn(async move {
+            loop {
+                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+
+                for (_, (terminal, app)) in clients.lock().await.iter_mut() {
+                    app.counter += 1;
+
+                    terminal
+                        .draw(|f| {
+                            f.render_widget(Clear, f.size());
+
+                            let title = Paragraph::new("terminal")
+                                .alignment(ratatui::layout::Alignment::Center)
+                                .style(Style::default().fg(Color::White));
+
+                            f.render_widget(title, centered_rect(f.size(), 50, 50));
+                        })
+                        .unwrap();
+                }
+            }
+        });
+
+        let config = Config {
+            inactivity_timeout: Some(std::time::Duration::from_secs(3600)),
+            auth_rejection_time: std::time::Duration::from_secs(3),
+            auth_rejection_time_initial: Some(std::time::Duration::from_secs(0)),
+            keys: vec![russh_keys::key::KeyPair::generate_ed25519().unwrap()],
+            ..Default::default()
+        };
+
+        self.run_on_address(Arc::new(config), ("0.0.0.0", 2222))
+            .await?;
+        Ok(())
+    }
+}
+
+impl Server for AppServer {
     type Handler = Self;
     fn new_client(&mut self, _: Option<std::net::SocketAddr>) -> Self {
         let s = self.clone();
@@ -55,7 +137,7 @@ impl server::Server for Server {
 }
 
 #[async_trait]
-impl server::Handler for Server {
+impl Handler for AppServer {
     type Error = anyhow::Error;
 
     async fn channel_open_session(
@@ -65,17 +147,33 @@ impl server::Handler for Server {
     ) -> Result<bool, Self::Error> {
         {
             let mut clients = self.clients.lock().await;
-            clients.insert((self.id, channel.id()), session.handle());
+            let terminal_handle = TerminalHandle {
+                handle: session.handle(),
+                sink: Vec::new(),
+                channel_id: channel.id(),
+            };
+
+            let backend = CrosstermBackend::new(terminal_handle.clone());
+            let mut terminal = Terminal::new(backend)?;
+            let width = terminal.size()?.width;
+            let height = terminal.size()?.height;
+            let rect = Rect {
+                x: 0,
+                y: 0,
+                width: width as u16,
+                height: height as u16,
+            };
+            terminal.resize(rect)?;
+            let app = App::new();
+
+            clients.insert(self.id, (terminal, app));
         }
+
         Ok(true)
     }
 
-    async fn auth_publickey(
-        &mut self,
-        _: &str,
-        _: &key::PublicKey,
-    ) -> Result<server::Auth, Self::Error> {
-        Ok(server::Auth::Accept)
+    async fn auth_publickey(&mut self, _: &str, _: &PublicKey) -> Result<Auth, Self::Error> {
+        Ok(Auth::Accept)
     }
 
     async fn data(
@@ -84,29 +182,49 @@ impl server::Handler for Server {
         data: &[u8],
         session: &mut Session,
     ) -> Result<(), Self::Error> {
-        let data = CryptoVec::from(format!("Got data: {}\r\n", String::from_utf8_lossy(data)));
-        self.post(data.clone()).await;
-        session.data(channel, data);
+        match data {
+            // Pressing 'q' closes the connection.
+            b"q" => {
+                self.clients.lock().await.remove(&self.id);
+                session.close(channel);
+            }
+            // Pressing 'c' resets the counter for the app.
+            // Only the client with the id sees the counter reset.
+            b"c" => {
+                let mut clients = self.clients.lock().await;
+                let (_, app) = clients.get_mut(&self.id).unwrap();
+                app.counter = 0;
+            }
+            _ => {}
+        }
+
         Ok(())
     }
 
-    async fn tcpip_forward(
+    /// The client's window size has changed.
+    async fn window_change_request(
         &mut self,
-        address: &str,
-        port: &mut u32,
-        session: &mut Session,
-    ) -> Result<bool, Self::Error> {
-        let handle = session.handle();
-        let address = address.to_string();
-        let port = *port;
-        tokio::spawn(async move {
-            let channel = handle
-                .channel_open_forwarded_tcpip(address, port, "1.2.3.4", 1234)
-                .await
-                .unwrap();
-            let _ = channel.data(&b"Hello from a forwarded port"[..]).await;
-            let _ = channel.eof().await;
-        });
-        Ok(true)
+        _: ChannelId,
+        col_width: u32,
+        row_height: u32,
+        _: u32,
+        _: u32,
+        _: &mut Session,
+    ) -> Result<(), Self::Error> {
+        {
+            let mut clients = self.clients.lock().await;
+            let (terminal, _) = clients.get_mut(&self.id).unwrap();
+            let rect = Rect {
+                x: 0,
+                y: 0,
+                width: col_width as u16,
+                height: row_height as u16,
+            };
+            terminal.clear();
+            terminal.resize(rect)?;
+        }
+
+        Ok(())
     }
 }
+
